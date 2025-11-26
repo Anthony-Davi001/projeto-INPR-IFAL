e1cd53dea11c0605eaf0ea3bbef875e0e3fa0ea7
Anthony Davi <anthonyvieira789@gmail.com>
2025-11-26 09:49:57 -0300

    Update mainloop.py

--- pokeapp/mainloop.py
+++ pokeapp/mainloop.py
@@ -2,6 +2,7 @@ import os
 from .ui import mostrar_menu_escolha, explicar_presente_simples
 from .utils import limpar_console
 from .pokeapp import iniciar_jogo_pokemon
+from .detetiveapp import iniciar_jogo_detetive
 
 def mainloop() -> None:
     while True:
@@ -12,7 +13,10 @@ def mainloop() -> None:
             case 2:
                 iniciar_jogo_pokemon()
             case 3: 
+                iniciar_jogo_detetive()
+            case 4:
                 break
             case _: 
                 pass
         limpar_console()
+
